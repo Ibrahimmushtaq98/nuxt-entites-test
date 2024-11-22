acf985deaca4ac5b3785e14817691a2782529904
Ibrahim Mushtaq <ibrahim.mushtaq@uoit.net>
2024-11-22 14:42:47 -0500

    Rest of the updates

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 
 // Use the auth composable to interact with authentication state
-const { signIn, signOut, getSession, status} = useAuth()
+const { signIn, signOut, getSession, status, data} = useAuth()
 const user = ref(null)
 
 // Fetch user session when the component is mounted
@@ -11,39 +11,22 @@ onMounted(async () => {
 })
 
 const handleLogin = async () => {
-  await signIn('identityserver', {external: true})
+  await signIn('github')
 }
-
-const setCookie = async () => {
-  try {
-    const response = await fetch('/api/set-cookies', {
-      method: 'GET', // Use POST if necessary
-      credentials: 'include', // Include cookies in the request
-    });
-    const data = await response.json();
-    console.log(data.message); // Log success message
-  } catch (error) {
-    console.error('Error setting cookie:', error);
-  }
-};
 </script>
 
 <template>
   <div class="page">
     <section class="flex flex-col gap-6">
-      <h1>Securing routes using Nuxt Auth</h1>
-      <p>
-        Wrapping your application with Nuxt Auth ensures all pages are secured.
-        Configuring subdomains or rewrites ensures they are all behind an
-        authentication wall.
-      </p>
+      <h1>Nuxt Auth</h1>
     </section>
 
     <hr class="border-t border-gray-300 my-6" />
 
     <section class="flex flex-col gap-3">
       <div v-if="status === 'authenticated'" class="flex flex-col gap-3">
-        <p>Welcome, {{ user?.role }}!</p>
+        <p>Logged in!</p>
+        <p>{{ data }}</p>
         <button class="btn" @click="signOut()">Sign out</button>
       </div>
 
@@ -53,14 +36,9 @@ const setCookie = async () => {
 
       <div v-else class="m-auto w-fit">
         <button class="btn-lg" @click="handleLogin()">
-          Sign in with IdentityServer
+          Sign in with Github
         </button>
       </div>
-    </section>
-    <section class="flex flex-col gap-3 mt-6">
-      <button class="btn-lg" @click="setCookie()">
-        Set Cookie
-      </button>
     </section>
   </div>
 </template>
--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -1,117 +1,43 @@
+
+import 'reflect-metadata'; // Required for TypeORM
+
 import { NuxtAuthHandler } from '#auth'
 import DuendeIDS6Provider from "next-auth/providers/duende-identity-server6"
 import { DuendeISUser } from 'next-auth/providers/duende-identity-server6';
 import GitHubProvider from 'next-auth/providers/github'
-import { TypeORMLegacyAdapter } from '@next-auth/typeorm-legacy-adapter';
+import * as entities from "~/external/entities"
+
 import { JWT } from 'next-auth/jwt';
+import { TypeORMAdapter } from '@auth/typeorm-adapter';
 
 const useSecureCookies = true;
-try{
-  const adapter = TypeORMLegacyAdapter({
-    type: 'mssql',
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT || "1433", 10),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    synchronize: true,
-    logging: true,
-    options: {
-      encrypt: true,
-    },
-  });
-  console.log('Adapter initialized:', adapter);
-}catch(error){
-  console.error(error);
-}
 
 export default NuxtAuthHandler({
     secret: process.env.AUTH_SECRET,
     providers: [
-        // GitHubProvider.default({
-        //   clientId: process.env.GITHUB_ID as string,
-        //   clientSecret: process.env.GITHUB_SECRET as string,
-        // }),
-        DuendeIDS6Provider.default({
-            // Id of the provider 
-            id: 'identityserver',
-            name: 'Labourly Identity Server',
-            
-            // Client value, this was set in Duende Identity and must follow that id
-            clientId: process.env.CLIENT_IDENTITY_ID,
-            clientSecret: process.env.CLIENT_IDENTITY_SECRET,
-            issuer: process.env.NUXT_PUBLIC_IDENTITY_BASE_URL,
-            authorization: {
-                url: `${process.env.NUXT_PUBLIC_IDENTITY_BASE_URL}/connect/authorize`,
-                params: {
-                  scope: 'openid profile api1 offline_access',
-                  response_type: 'code',
-                },
-            },
-            token: {
-                url: `${process.env.NUXT_PUBLIC_IDENTITY_BASE_URL}/connect/token`,
-                params: {
-                  grant_type: 'authorization_code',
-                  redirect_uri: `${process.env.NUXT_PUBLIC_FRONTEND_SECONDARY_BASE_URL}/api/auth/callback/identityserver`
-                }
-            },
-            userinfo: `${process.env.NUXT_PUBLIC_IDENTITY_BASE_URL}/connect/userinfo`,
-            //checks: ['pkce'],
-            checks: [],
-            idToken: true,
-            profile(profile: DuendeISUser, token: JWT) {
-                return {
-                  id: profile.sub,
-                  name: profile.name,
-                  email: profile.email,
-                  token: token
-                };
-            },
-            // Force HTTPS on redirect and callback
-            redirect: process.env.NUXT_PUBLIC_FRONTEND_SECONDARY_BASE_URL,
+        GitHubProvider.default({
+          clientId: process.env.GITHUB_ID as string,
+          clientSecret: process.env.GITHUB_SECRET as string,
         }),
     ],
-    // adapter: TypeORMLegacyAdapter({
-    //   type: 'mssql',
-    //   host: process.env.DB_HOST,
-    //   port: parseInt(process.env.DB_PORT || "1433", 10),
-    //   username: process.env.DB_USERNAME,
-    //   password: process.env.DB_PASSWORD,
-    //   database: process.env.DB_NAME,
-    //   connectionTimeout: 3000,
-    //   synchronize: true,
-    //   options: {
-    //     encrypt: true,
-    //   },
-    // }),
-    // cookies: {
-    //     sessionToken: {
-    //       name: `${useSecureCookies ? '__Secure-' : ''}next-auth.session-token`,
-    //       options: {
-    //         httpOnly: true,
-    //         sameSite: 'none',
-    //         path: '/',
-    //         domain: '.laborly.io',
-    //         secure: useSecureCookies,
-		// 	//maxAge: 60 * 5,
-		// 	//expires: new Date(Date.now() + 5 * 60 * 1000)
-    //       },
-    //     },
-    // },
-	// callbacks: {
-	// 	async jwt({ token, account, profile }){
-	// 	  // When a new session is created, save role and name from profile
-	// 	  if (account && profile) {
-	// 		token.accessToken = account.access_token;
-	// 		token.refreshToken = account.refresh_token;
-	// 		token.id_token = account.id_token;
-	// 		token.accessTokenExpires = account.expires_at ? account.expires_at * 1000 : Date.now() + 3600 * 1000;
-	// 		token.name = profile.name;
-	// 		token.role = profile.role || 'user'; // Store role in the token
-	// 	  }
-	// 	  return token;
-	// 	},
-	//   },
+    adapter: TypeORMAdapter({
+      type: 'mssql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT!),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      connectionTimeout: 3000,
+      entities: entities,
+      synchronize: false, // DONT ALLOW TRUE IN PROD
+      dropSchema: false, // DONT ALLOW TRUE IN PROD
+      logging: false,
+      options: {
+        encrypt: true,
+      },
+      extra:{
+        trustServerCertificate: false
+      }
+    }),
     debug: true,
-    useSecureCookies: true,
 })
